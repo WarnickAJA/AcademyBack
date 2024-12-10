384432ccaa7881f0e50fddcb46e430338d371109
WarnickAJA <javierq666@gmail.com>
2024-12-10 16:49:30 -0300

    update playlist

--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -8,6 +8,33 @@ const createCourse = async (courseData) => {
 
 // Obtener todos los cursos
 const getCourses = async () => {
+  Courses.updateMany(
+    {},
+    {
+      $set: {
+        playlist: [
+          {
+            name: "Top 3 Cursos de Programación en Español y GRATIS",
+            url: "https://www.youtube.com/watch?v=acuU_LRH094",
+          },
+          {
+            name: "Fundamentos de programación para principiantes desde cero",
+            url: "https://www.youtube.com/playlist?list=PLmIB7uA74VvawrhX2PI8klvIv-OMZsM4y",
+          },
+          {
+            name: "Curso de PROGRAMACIÓN DESDE CERO | Crea tus PRIMEROS PROGRAMAS",
+            url: "https://www.youtube.com/playlist?list=PLAWADOpEOsdGBc8tYyW_im4Fg5z1m6eZu",
+          },
+        ],
+      },
+    }
+  )
+    .then((result) => {
+      console.log("Documentos actualizados con un nuevo campo vacío:", result);
+    })
+    .catch((error) => {
+      console.error("Error al actualizar documentos:", error);
+    });
   return await Course.find();
   //return await Course.find().populate('instructor', 'name email'); // Poblamos instructor
 };

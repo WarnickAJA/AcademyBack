d192b6c4e6f9fa19d9bcf4d3fe4e38c1a1c7a1a1
WarnickAJA <javierq666@gmail.com>
2024-12-10 16:08:18 -0300

    fix

--- backend/src/models/Course.js
+++ backend/src/models/Course.js
@@ -1,71 +1,73 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   description: {
     type: String,
-    required: true
+    required: true,
   },
   category: {
     type: String,
-    enum: ['Marketing', 'Programación', 'Diseño', 'Otros'], // Puedes agregar más categorías
-    required: true
+    enum: ["Marketing", "Programación", "Diseño", "Otros"], // Puedes agregar más categorías
+    required: true,
   },
   subcategory: String,
   duration: {
     type: Number, // En horas o semanas
-    default: 0
+    default: 0,
   },
   price: {
     type: Number,
-    default: 0
+    default: 0,
   },
   instructor: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User' // Referencia a la colección de usuarios
+    ref: "User", // Referencia a la colección de usuarios
   },
   startDate: Date,
   endDate: Date,
   status: {
     type: String,
-    enum: ['active', 'inactive', 'upcoming'],
-    default: 'active'
+    enum: ["active", "inactive", "upcoming"],
+    default: "active",
   },
   //requirements: [String],
   level: {
     type: String,
-    enum: ['beginner', 'intermediate', 'advanced']
+    enum: ["beginner", "intermediate", "advanced"],
   },
   image: String, // URL de la imagen
   video: String, // URL del video
-//   modules: [{
-//     name: String,
-//     description: String,
-    playlist: [{
+  //   modules: [{
+  //     name: String,
+  //     description: String,
+  playlist: [
+    {
       name: String,
-       url: String, // Puedes almacenar diferentes tipos de contenido (texto, video, etc.)
-     // Duración en minutos
-   }]
-//   }],
-//   reviews: [{
-//     user: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: 'User'
-//     },
-//     rating: Number,
-//     comment: String
-//   }],
+      url: String, // Puedes almacenar diferentes tipos de contenido (texto, video, etc.)
+      // Duración en minutos
+    },
+  ],
+  //   }],
+  //   reviews: [{
+  //     user: {
+  //       type: mongoose.Schema.Types.ObjectId,
+  //       ref: 'User'
+  //     },
+  //     rating: Number,
+  //     comment: String
+  //   }],
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   updatedAt: {
-    type: Date
-  }
+    type: Date,
+  },
 });
 
-module.exports = mongoose.model('Course', courseSchema);
+module.exports = mongoose.model("Course", courseSchema);

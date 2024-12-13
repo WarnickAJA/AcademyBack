626e34892bd173c3aa4df2620fd7aa8ec873c51d
WarnickAJA <javierq666@gmail.com>
2024-12-12 23:54:14 -0300

    test

--- backend/src/payments/mercadopago.js
+++ backend/src/payments/mercadopago.js
@@ -6,11 +6,18 @@ const client = new MercadoPagoConfig({
 const createPreference = async (req, res, next) => {
   try {
     const body = {
-      items: req.body.map((product) => ({
-        title: product.name,
-        unit_price: Number(product.price),
-        quantity: Number(product.quantity),
-      })),
+      //   items: req.body.map((product) => ({
+      //     title: product.name,
+      //     unit_price: Number(product.price),
+      //     quantity: Number(product.quantity),
+      //   })),
+      items: [
+        {
+          title: "req.body.name",
+          unit_price: 250,
+          quantity: 5,
+        },
+      ],
       back_urls: {
         success: "http://martin-juncos.github.io/success",
         failure: "http://martin-juncos.github.io/failure",
